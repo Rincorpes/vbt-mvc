942bacff412eeae7f18269ba9a89a67508c6312d
Santiago Rincón <rincorpes@gmail.com>
2017-03-19 10:24:12 -0400

    Hello World!

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Front of the vbt application. This file doesn't make anything but loads
+ * the vbt core to make the system works
+ *
+ * @copyright 2015 - 2017 Vbt
+ * @author Santiago Rincón <rincorpes@gmail.com>
+ * @link https://twitter.com/rincorpes
+ * @version 1.0.0
+ */
+
+echo 'Hola Mundo!';
+?>
